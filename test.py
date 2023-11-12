39428e756927452c8e2331b6e8f8ee4b0b75a670
AdrianNoces <142280131+AdrianNoces@users.noreply.github.com>
2023-11-12 14:36:18 +0000

    update

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print('hrloo')
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyKivyApp(App):
+    def build(self):
+        # Create a button with a callback function
+        button = Button(text='Press me!', on_press=self.on_button_press)
+        return button
+
+    def on_button_press(self, instance):
+        print("Button pressed!")
+
+# Run the app
+if __name__ == '__main__':
+    MyKivyApp().run()
